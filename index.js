42dcdb787e146ca6d434d534102f327be282a6db
Sazib <mahiburislamsazib@gmail.com>
2024-07-03 12:56:25 +0600

    task-4

--- index.js
+++ index.js
@@ -8,3 +8,28 @@ bar('foo')
 foo()
 
 //task-2
+
+function even(x) {
+    return x % 2 ==0;
+}
+
+function obb(x) {
+    return x % 2 ==1;
+}
+console.log();
+
+
+//task-4
+
+var color ="green"
+
+switch (color) {
+    case 'red': console.log('stop don,t move');
+        break;
+    case 'yellow': console.log('wait for green signal');
+        break;
+    case 'green': console.log('crose rode');
+        break;
+
+    default: console.log('please maintain signal. those "red", "yellow", "green";');
+}
